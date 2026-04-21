685c72135be11a8d60b25dfcd38299692ed77494
Nycolas Felix <nycolasfelix.31@gmail.com>
2026-04-21 10:10:53 -0300

    Add count of people

--- semana1/exercicio2/Program.py
+++ semana1/exercicio2/Program.py
@@ -11,15 +11,17 @@ class Program:
     print("Input the number of people you want to add: ")
     n = int(input())
     
-    for _ in range(n):
+    for i in range(1, n + 1):
+        print(f"\nInput the data of the {i}º person: ")
         name = input("Whats your name? ")
         age = int(input("Whats your age? "))
         people = Peoples(name, age)
         manager.add(people)
       
-    print("List of all people: ")
+    print("\nList of all people: ")
     for people in manager.List_all():
         print(f"Name: {people.name}\nAge: {people.age}\nAge Range: {people.AgeRange.value}\nid: {people.id}\n")
 
 if __name__ == "__main__":
     Program.main()
+    inout = input("Press enter to exit...")
